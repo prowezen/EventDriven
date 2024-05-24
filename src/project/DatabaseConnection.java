96cbbde51184e3c809a9b111b6cc406cb6be7faa
User <User@DESKTOP-LOC4SSC>
2024-05-25 07:36:53 +0800

    house cleaning

--- src/project/DatabaseConnection.java
+++ src/project/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/proj_mgmt";
+    private static final String URL = "jdbc:mysql://localhost:3306/proj_mgmt_db";
     private static final String USER = "myuser";
     private static final String PASSWORD = "1234";
 
